dbba491dd804979a0fb830e70dfd43ca6057cf4c
boilWater <1549793329@qq.com>
2016-05-31 14:02:51 +0800

    Initial Commit

---
+++ AnimationDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDome
+//
+//  Created by liangbai on 16/5/31.
+//  Copyright © 2016年 liangbai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDome
+//
+//  Created by liangbai on 16/5/31.
+//  Copyright © 2016年 liangbai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
